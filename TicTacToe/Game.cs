49cf00808125b465cfbf65c3b2fc233c9796fa0c
shabialili <sebnem.eliyeva1998@gmail.com>
2022-04-01 02:18:01 +0400

    TicTacToe

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        public string CurrentPlayer { get; set; } = X;
+        private const string X = "X";
+        private const string O = "O";
+        private string[,] Board = new string[3, 3];
+
+        public void SetNextPlayer()
+        {
+            if(CurrentPlayer == X)
+            {
+                CurrentPlayer = O;
+            }
+            else
+            {
+                CurrentPlayer = X;  
+            }
+        }
+        public bool PlayerWin()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(Board[i, 0]))
+                {
+                    if (Board[i, 0] == Board[i, 1] && Board[i, 0] == Board[i, 2])
+                    {
+                        return true;
+                    }
+                }
+            }
+            for (var i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(Board[0, i]))
+                {
+                    if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (!String.IsNullOrWhiteSpace(Board[1, 1]))
+            {
+                if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+                {
+                    return true;
+                }
+
+                if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+                {
+                    return true;
+                }
+            }
+            return false;  
+        }
+
+        internal void UpdateBoard(Position position, string value)
+        {
+            Board[position.x,position.y] = value;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Game gameLogic = new Game(); 
+
+        private void PlayerClickSpace(object sender, RoutedEventArgs e)
+        {
+            var space = (Button)sender;
+            if (!String.IsNullOrWhiteSpace(space.Content?.ToString())) return;
+            space.Content = gameLogic.CurrentPlayer;
+
+
+            var coodinates = space.Tag.ToString().Split(',');  
+            var xValue = int.Parse(coodinates[0]);
+            var yValue = int.Parse(coodinates[1]);
+
+            var buttonPosition = new Position() 
+            { 
+                x = xValue, 
+                y = yValue 
+            };
+            gameLogic.UpdateBoard(buttonPosition, gameLogic.CurrentPlayer);
+
+            if (gameLogic.PlayerWin())
+            {
+                WinScreen.Text = $"{gameLogic.CurrentPlayer} WINS!!!";
+                WinScreen.Visibility = Visibility.Visible;
+            }
+            
+            gameLogic.SetNextPlayer();
+        }
+        private void buttonNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(var control in gridBoard.Children)
+            {
+                if(control is Button)
+                {
+                    ((Button)control).Content = String.Empty;   
+                }
+            }
+            gameLogic = new Game();
+            WinScreen.Visibility = Visibility.Collapsed; 
+        }
+    }
+}
